e36fd26b21cd71798884c1c875201e74e878c2b6
yuxuwu <ywu148@binghamton.edu>
2018-08-14 16:17:23 -0400

    Cleaned up sidebar toggle render code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { matchPath, withRouter } from "react-router-dom";
-import MediaQuery from 'react-responsive';
 import PropTypes from 'prop-types';
 
 import Sidebar from "./components/sidebar";
@@ -27,30 +26,17 @@ class App extends Component {
 	}
 
 	render() {
+		let iconClassName = this.state.sidebarActive ? "sidebar__icon--active" : "sidebar__icon";
 		return (
 			<div className="container">
 				{/*Sidebar Toggle for screens <= 900px
 				  *Changes icon based on if the sidebar is active
 				  */}
-				<MediaQuery query="(max-width: 900px)">
-					{(matches) => {
-						if(matches && this.state.sidebarActive) {
-							return (
-								<div className="sidebar__button--active">
-									<span className="sidebar__icon">&nbsp;</span>
-								</div>
-							);
-						} else if(matches && !this.state.sidebarActive) {
-							return (
-								<div className="sidebar__button">
-									<span className="sidebar__icon">&nbsp;</span>
-								</div>
-							);
-						} else {
-							return (<div>&nbsp;</div>); // i hate that i have to do this
-						}
-					}}
-				</MediaQuery>
+				{/*<input type="checkbox" className="sidebar__checkbox" id="navi-toggle"/>*/}
+				<div onClick={() => { this.setState({sidebarActive: !(this.state.sidebarActive)})} } className="sidebar__button">
+					<span className={iconClassName}>&nbsp;</span>
+				</div>
+
 				<Sidebar
 					onLabelSelect={selectedLabel => { this.setState({ selectedLabel, sidebarActive: false }) }}
 					labels={this.state.labels}
