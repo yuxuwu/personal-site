e168101007344fdeea954729bcbf8641101bdb0d
yuxuwu <yuxu.wu1@gmail.com>
2020-01-25 21:31:32 -0500

    Hello p5 ;)

--- src/app/yu-panel/yu-panel.component.ts
+++ src/app/yu-panel/yu-panel.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as p5 from 'p5';
+
 @Component({
-  selector: 'yu-panel',
-  templateUrl: './yu-panel.component.html',
-  styleUrls: ['./yu-panel.component.scss']
+	selector: 'yu-panel',
+	templateUrl: './yu-panel.component.html',
+	styleUrls: ['./yu-panel.component.scss']
 })
 export class YuPanelComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		const sketch = (s) => {
+			s.setup = () => {
+				s.createCanvas(400, 400);
+				console.log("We're in baby ;)");
+			}
+			s.draw = () => {
+				s.background(255);
+				s.rect(100, 100, 100, 100);
+			}
+		}
+
+		let canvas = new p5(sketch);
+	}
 
 }
